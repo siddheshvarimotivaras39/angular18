18aa79e59bea4cedaf1db93d12b98e2b15a9b487
siddheshvarimotivaras39 <motivarassiddheshvari@gmail.com>
2024-07-09 14:34:30 +0530

    login with localstorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular_18_login_with_local_storage';
+  title = 'login_with_local_storage';
 }
